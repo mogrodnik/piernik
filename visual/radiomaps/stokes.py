61c3abc4c6e58ae8c45c0a2436c57c57a2f701a8
mogrodnik <mateusz.ogrodnik@astri.uni.torun.pl>
2021-07-25 19:55:55 +0200

    [stokes] Modify indexation of Ecre and Ncre to work properly either with read_data / yt_read_data

--- visual/radiomaps/stokes.py
+++ visual/radiomaps/stokes.py
@@ -82,14 +82,14 @@ def stokes_params(Bp,Bq,Bn,rho_ion,Ecrp,wave_data,ds,n3,**kwargs):
       elif stg.mode == 'spectral':
          for i3 in range(n3):
             #elfq = electrons.crenpp(nu_s, ncre, B_perp[i3], Ecre[:,i3])            # DEPRECATED
-            elfq = electrons.crenppfq(nu_s, ncre, B_perp[i3], Ecre[:,i3], Ncre[:,i3])
+            elfq = electrons.crenppfq(nu_s, ncre, B_perp[i3], Ecre[:][i3], Ncre[:][i3])
             I[i3] = np.sqrt(nu_s*B_perp[i3]) * elfq
 
          if stg.print_SI:
             I2 = np.zeros_like(B_perp)
             for i3 in range(n3):
                #elfq2 = electrons.crenpp(nu_2, ncre, B_perp[i3], Ecre[:,i3])        # DEPRECATED
-               elfq2 = electrons.crenppfq(nu_2, ncre, B_perp[i3], Ecre[:,i3], Ncre[:,i3])
+               elfq2 = electrons.crenppfq(nu_2, ncre, B_perp[i3], Ecre[:][i3], Ncre[:][i3])
                I2[i3] = np.sqrt(nu_2*B_perp[i3]) * elfq2
 
    if stg.print_PI or stg.print_SI or stg.print_vec:
