e5fa0e208c662d0be5fd1f3a396c4053e052b3e0
mogrodnik <mogrodnik@umk.pl>
2021-10-22 13:14:25 +0200

    [yt_read_data] Fix ranges: attribute ranges coordinates to cell centers. Fixes data-shift present in previous versions

--- visual/radiomaps/yt_read_data.py
+++ visual/radiomaps/yt_read_data.py
@@ -125,7 +125,7 @@ def data_h5_yt(filename, ax_set, wave_data, imresw, imdepth):
    # Prepare the limits of plotted area to be returned, NOTICE formally these are the same as provided by settings.plotext
    x = np.linspace(rbeg[i_w] / 1000., rend[i_w] / 1000., imres[0])
    y = np.linspace(rbeg[i_h] / 1000., rend[i_h] / 1000., imres[1])
-   figext = [(rbeg[i_w])/ 1000., (rend[i_w])/ 1000., (rbeg[i_h] - 0.5 * dw)/ 1000., (rend[i_h] - 0.5 * dw)/ 1000.]
+   figext = [(rbeg[i_w])/ 1000., (rend[i_w])/ 1000., (rbeg[i_h])/ 1000., (rend[i_h])/ 1000.]
 
    # Prepare variable coordinates for iteration in domain
    rbegv = [0, 0, 0]
@@ -134,8 +134,11 @@ def data_h5_yt(filename, ax_set, wave_data, imresw, imdepth):
    rendv[:] = rbeg[:]            # NOTICE only coordinate at ax_set is supposed to vary here between rbegv and rendv
    rendv[ax_set] = rend[ax_set]  # NOTICE remaining coordinates will start to vary during iteration
 
+   rbeg[i_w] = rbeg[i_w] + 0.5 * dw
+   rbeg[i_h] = rbeg[i_h] + 0.5 * dh
    rbegv[i_w] = rbegv[i_w] + 0.5 * dw
    rbegv[i_h] = rbegv[i_h] + 0.5 * dh
+
    print('Sendig data to map computation \n')
    # This part should works regardless of chosen ax_set
    for i in range(imres[0]):
